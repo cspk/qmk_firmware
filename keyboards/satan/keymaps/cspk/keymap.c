c351ea52403086024e781f7dbd4ecf3aa3c05bb3
Pavel Serebryakov <serebryakovp@progressufa.ru>
2018-11-28 13:17:30 +0500

    satan/cspk: use tap dance to switch layers

--- keyboards/satan/keymaps/cspk/keymap.c
+++ keyboards/satan/keymaps/cspk/keymap.c
@@ -7,11 +7,22 @@
 
 #define _______ KC_TRNS
 
+void td_fn1_fn2_finished(qk_tap_dance_state_t *state, void *user_data);
+void td_fn1_fn2_reset(qk_tap_dance_state_t *state, void *user_data);
+
+enum tap_dance {
+	TD_FN1_FN2
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+	[TD_FN1_FN2] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_fn1_fn2_finished, td_fn1_fn2_reset)
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[LAYER_BASIC] = LAYOUT_60_ansi(
 		KC_ESC,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC, \
 		KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS, \
-		MO(LAYER_FUNCTION), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,  KC_ENT, \
+		TD(TD_FN1_FN2), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,  KC_ENT, \
 		KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT, \
 		KC_LCTL, KC_LGUI,KC_LALT,   KC_SPC,   KC_RALT,KC_RGUI, KC_APP ,KC_RCTL
 	),
@@ -20,8 +31,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TILD,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_DEL, \
 		_______,_______,_______,_______,_______,_______,_______,KC_MPRV,KC_MPLY,KC_MSTP,KC_MNXT, KC_VOLD, KC_VOLU, KC_MUTE, \
 		_______,_______,_______,_______,_______,_______,KC_LEFT,KC_DOWN,KC_UP,KC_RGHT,_______,KC_GRV ,_______, \
-		MO(LAYER_PGUPDOWN),_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,MO(LAYER_PGUPDOWN), \
-		_______,_______,MO(LAYER_NUMBERS),  KC_CAPS,  _______,_______,KC_INS,RESET
+		_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, \
+		_______,_______,_______,  KC_CAPS,  _______,_______,KC_INS,RESET
 	),
 
 	[LAYER_PGUPDOWN] = LAYOUT_60_ansi(
@@ -40,3 +51,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______,_______,_______,KC_0,_______,_______,_______,_______
 	),
 };
+
+void td_fn1_fn2_finished(qk_tap_dance_state_t *state, void *user_data) {
+	if (state->count == 1) {
+		layer_on(LAYER_FUNCTION);
+	}
+	else if (state->count == 2) {
+		layer_on(LAYER_PGUPDOWN);
+	}
+	else {
+		layer_on(LAYER_NUMBERS);
+	}
+}
+
+void td_fn1_fn2_reset(qk_tap_dance_state_t *state, void *user_data) {
+	if (state->count == 1) {
+		layer_off(LAYER_FUNCTION);
+	}
+	else if (state->count == 2) {
+		layer_off(LAYER_PGUPDOWN);
+	}
+	else {
+		layer_off(LAYER_NUMBERS);
+	}
+}
